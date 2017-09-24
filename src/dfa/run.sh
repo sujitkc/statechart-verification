2e1ad927a8fe631e4337d2175a5cd251dfa2c0fa
sujitkc <sujitkc@gmail.com>
2017-09-24 15:51:53 +0530

    Modified Makefile and run.sh to generate .class files in classes/ directory. 	modified:   Makefile 	modified:   run.sh

--- src/dfa/run.sh
+++ src/dfa/run.sh
@@ -1,3 +1,3 @@
 MYLIBPATH=/home/sujit/My-Downloads/source/jflex-1.6.0/lib
 
-java -cp ".:"${MYLIBPATH}"/java-cup-11a.jar" Analyser $1
+java -cp "./classes/:"${MYLIBPATH}"/java-cup-11a.jar" Analyser $1
