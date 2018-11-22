ff9491e7b020eb1a74481f1e12bc13db8f8138ee
karthiv <karthiv.sw@gmail.com>
2018-11-22 07:09:08 +0530

    building stablcfg

--- src/dfa/stablcfg/CFEdge.java
+++ src/dfa/stablcfg/CFEdge.java
@@ -1,28 +1,86 @@
 package stablcfg;
 
 import cfg.*;
+import utilities.IdGenerator;
 public class CFEdge implements ICFEdge {
-	public ICFGNode getHead(){ //target node
-		return null;
-	} 
-	public ICFGNode getTail(){ //source node
-		return null;
-	} 
-	public void setHead(ICFGNode node){ //target node
-	
-	} 
-	public void setTail(ICFGNode node){ //source node
-	
-	} 
-	public ICFG getCFG(){
-		return null;
+
+	ICFG mCFG;
+	ICFGNode mTail = null;
+	ICFGNode mHead = null;
+	private String mId;
+	public CFEdge(ICFG cfg, ICFGNode tail, ICFGNode head) {
+		this.mCFG = cfg;
+		this.mTail = tail;
+		this.mHead = head;
+		if(cfg != null) {
+//			cfg.addEdge(this);
+		}
+		this.mId = CFEdge.generateId();
 	}
-	public void setCFG(ICFG graph){}
-	public void setEdgeAnnotation(boolean bool){}
+	private static String generateId() {
+		return IdGenerator.generateId("CFEdge");
+	}
+
+public CFEdge(String id, ICFG cfg, ICFGNode tail, ICFGNode head) throws Exception {
+		this.mCFG = cfg;
+		this.mTail = tail;
+		this.mHead = head;
+		if(IdGenerator.hasId(id)) {
+			Exception e = new Exception("Can't construct CFEdge : something with name '" + id + "' already exists.");
+			throw e;			
+		}
+		if(cfg != null) {
+			cfg.addEdge(this);
+		}
+		IdGenerator.addId(id);
+		this.mId = id;
+	}
+
+
+	@Override
+	public ICFGNode getHead() {
+		return this.mHead;
+	}
+
+	@Override
+	public ICFGNode getTail() {
+		return this.mTail;
+	}
+
+	@Override
+	public ICFG getCFG() {
+		return this.mCFG;
+	}
+
+	@Override
+	public void setCFG(ICFG graph) {
+		this.mCFG = graph;
+	}
+
+	@Override
+	public String getId() {
+		return this.mId;
+	}
+	
+	public String toString() {
+		return "CFEdge " + this.mId + " tail = " + this.mTail.getId() + " head = " + this.mHead.getId() + "\n";
+	}
+
+	@Override
+	public void setHead(ICFGNode node) {
+		this.mHead = node;
+	}
+
+	@Override
+	public void setTail(ICFGNode node) {
+		this.mTail = node;
+	}	
+
+
+	
+
+public void setEdgeAnnotation(boolean bool){}
 	public boolean getEdgeAnnotation(){
 		return false;
-	}
-	public String getId(){
-		return null;
 	}
 }
