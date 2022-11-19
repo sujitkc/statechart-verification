180289177171acb147f71d8d45079c29e60b4672
karthiv <karthiv.sw@gmail.com>
2022-11-19 19:57:21 +0530

    eventqueue

--- src/dfa/constablsim/StatechartSimulator.java
+++ src/dfa/constablsim/StatechartSimulator.java
@@ -1,10 +1,21 @@
 package constablsim;
-
+import ast.*;
+import java.util.*;
+import constablsim.ast.*;
 public class StatechartSimulator extends Simulator {
-    public void initialize(){
-
+    private Statechart statechart = null;
+    private List<String> eventQueue=null;
+    private Configuration activeconfig=null;
+    EventQueue eq=new EventQueue();
+    //private final String tinit=EventQueue.tinit;
+    public void simulate(){
+        for(String ev : eq.eventQueue){
+            consumeEvent(ev);   
+        }
     }
-    public void consumeEvent(){
+    public void consumeEvent(String event){
+        //detectNonDeterminism
+        //computeCode
         
     }
 }
