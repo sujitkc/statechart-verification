e88e4abb9bfe79ac45138e0b69e5907207088ef2
karthiv <karthiv.sw@gmail.com>
2023-06-14 02:39:25 +0530

    testscfuzzer

--- src/dfa/testcases/simulator2/TestSingleSCFuzzer.java
+++ src/dfa/testcases/simulator2/TestSingleSCFuzzer.java
@@ -45,7 +45,8 @@ public class TestSingleSCFuzzer {
   	//makeTestCase("data/constabl_actions/1_source_atomic/t1_1.stbl", new String[] {"A"}, new String[]{"B"});
   	//makeTestCase("data/uwfms/CollisionAvoidance.stb", new String[] {"A"}, new String[]{"B"});
   //	makeTestCase("data/uwfms/EmergencyVehicleAvoidance.stb", new String[] {"A"}, new String[]{"Fail", "Fail"});
-  		makeTestCase("data/uwfms/eva_speedctrl.stb", new String[] {"A"}, new String[]{"Fail", "Fail"});
+  		//makeTestCase("data/uwfms/eva_speedctrl.stb", new String[] {"A"}, new String[]{"Fail", "Fail"});
+  		makeTestCase("data/uwfms/caeva.stb", new String[] {"A"}, new String[]{"Disengaged","Disengaged1"});
   /*	makeTestCase("data/constabl_actions/1_source_atomic/t1_2#1.stbl", new String[] {"A"}, new String[]{"B1"});
   	makeTestCase("data/constabl_actions/1_source_atomic/t1_2#2.stbl", new String[] {"A"}, new String[]{"B11"});
   	makeTestCase("data/constabl_actions/1_source_atomic/t1_2#3.stbl", new String[] {"A"}, new String[]{"A1","A2"});
