db559fb26f6af5cd097d9720c5484e6eae78a583
karthiv <karthiv.sw@gmail.com>
2023-06-14 15:41:48 +0530

    reduce no of events

--- src/dfa/testcases/simulator2/TestSingleSCFuzzer.java
+++ src/dfa/testcases/simulator2/TestSingleSCFuzzer.java
@@ -353,7 +353,7 @@ public class TestSingleSCFuzzer {
 		
 		List<String> list=new ArrayList<String>();
 		Random r=new Random();
-		for(int j=0;j<100;j++){
+		for(int j=0;j<1000;j++){
 			//num=data.consumeInt​(0,2);
 			
 			num=r.nextInt(5);
